79aa489c93fb11abacb750b599ee2cd432755820
Amresh Verma <geniousamresh@gmail.com>
2020-02-15 13:33:33 +0530

    Update ReverseStringWithoutSpecialChar.java

--- ReverseStringWithoutSpecialChar.java
+++ ReverseStringWithoutSpecialChar.java
@@ -1,1 +1,38 @@
-v
+package Array;
+
+public class ReverseStringWithoutSpecialChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "a!!!b.c.d,e'f,ghi";
+		char[] charArray = str.toCharArray();
+
+		System.out.println("Input string: " + str);
+		reverse(charArray);
+		String revStr = new String(charArray);
+
+		System.out.println("Output string: " + revStr);
+
+	}
+
+	public static void reverse(char[] str) {
+		int l = 0;
+		int r = str.length - 1;
+		while (l < r) {
+			if (!Character.isAlphabetic(str[l]))
+				l++;
+			else if (!Character.isAlphabetic(str[r]))
+				r--;
+			else {
+				char temp = str[l];
+				str[l] = str[r];
+				str[r] = temp;
+				l++;
+				r--;
+			}
+		}
+
+	}
+
+}
