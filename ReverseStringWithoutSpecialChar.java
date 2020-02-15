830aa1588e96b53da8c461c1dfe180b47ed2ae01
Amresh Verma <geniousamresh@gmail.com>
2020-02-15 13:44:39 +0530

    Update ReverseStringWithoutSpecialChar.java

--- ReverseStringWithoutSpecialChar.java
+++ ReverseStringWithoutSpecialChar.java
@@ -1,3 +1,13 @@
+/*
+1) Let input string be 'str[]' and length of string be 'n'
+2) l = 0, r = n-1
+3) While l is smaller than r, do following
+    a) If str[l] is not an alphabetic character, do l++
+    b) Else If str[r] is not an alphabetic character, do r--
+    c) Else swap str[l] and str[r]
+    
+	*/
+
 package Array;
 
 public class ReverseStringWithoutSpecialChar {
