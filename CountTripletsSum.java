bd2b5fc37f5d3c9accbe8f1318be6856fb2138cd
Amresh Verma <geniousamresh@gmail.com>
2020-02-15 14:40:32 +0530

    Update CountTripletsSum.java

--- CountTripletsSum.java
+++ CountTripletsSum.java
@@ -1,1 +1,36 @@
-f
+package Array;
+
+public class CountTripletsSum {
+	
+	static int arr[] = new int[]{5, 1, 3, 4, 7}; 
+	
+	public static void main(String[] args) {
+		countTriplets(arr,12);
+	}
+	
+	public static void countTriplets(int arr[],int sum)
+	{
+		int n=arr.length;
+		int count=0;
+		
+		for(int i=0;i<n-2;i++)
+		{
+			for(int j=i+1;j<n-1;j++)
+			{
+				for(int k=j+1;k<n;k++)
+				{
+					
+					if(arr[i]+arr[j]+arr[k]<sum)
+						count++;
+				}
+			}
+		}
+		
+		System.out.println("total count=="+count);
+		
+	}
+
+}
+
+
+//===========================another method=================================
