cf7236485d2e258967024da9c86a30eba5d615cc
Amresh Verma <geniousamresh@gmail.com>
2024-02-14 20:08:34 +0530

    Create Second_Approch_OneDArray.java

---
+++ Second_Approch_OneDArray.java
@@ -0,0 +1,39 @@
+class OneDArray
+{
+	public static void main(String args[])
+	{
+	 	int n, i,j;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number of the element");
+		n=s.nextInt();
+		int a[]=new int[n];
+		int b[]=new int[n+1]
+		for( i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		// insertig element in second array
+		System.out.println("Enter the index where you want place new value");
+		int m=sc.nextInt();
+		System.out.println("Enter the new element value");
+		int newValue=sc.nextInt();
+		for(j=0;j<n+1;j++)
+		{
+			if(j==m){
+				b[j]=newValue;
+			}else if(j<m){  
+				b[j]=a[j]
+			}else{
+				b[j]=a[j-1]
+			}			
+		}
+
+		System.out.println("printing the element of array");
+		for(i=0;i<n+1;i++)
+		{
+			System.out.println(b[i]);
+		}
+
+	}
+
+}
