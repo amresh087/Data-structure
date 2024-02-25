45424e8aac4a3174d7cbb68a9ac7477d34fd7af2
Amresh Verma <geniousamresh@gmail.com>
2024-02-25 10:05:22 +0530

    Update Deletion_OneDArray.java

--- Deletion_OneDArray.java
+++ Deletion_OneDArray.java
@@ -1,30 +1,34 @@
-class Deletion_OneDArray
+package com.array.demo;
+
+import java.util.Scanner;
+
+public class Deletion_OneDArray
 {
 	public static void main(String args[])
 	{
 	 	int n, i,j;
-		Scanner s=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number of the element");
-		n=s.nextInt();
+		n=sc.nextInt();
 		int a[]=new int[n];
-		int b[]=new int[n-1]
+		int b[]=new int[n-1];
 		for( i=0;i<n;i++)
 		{
-			a[i]=s.nextInt();
+			a[i]=sc.nextInt();
 		}
 		// insertig element in second array
 		System.out.println("Enter the index where you want to delete value");
 		int m=sc.nextInt();
 		System.out.println("Enter the new element value");
 		int newValue=sc.nextInt();
-		for(j=0;j<n-1;j++)
+		for(j=0;j<n+1;j++)
 		{
 			if(j==m){
 				continue;
 			}else if(j<m){  
-				b[j]=a[j]
+				b[j]=a[j];
 			}else{
-				b[j-1]=a[j]
+				b[j-1]=a[j];
 			}			
 		}
 
